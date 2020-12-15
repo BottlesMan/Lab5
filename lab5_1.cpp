ceda3a7426de882cdb826028f484fdae6dfafa0a
BottlesMan <man2545_@hotmail.com>
2020-12-15 17:07:18 +0700

    "Send 3 Files"

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+    int N;
+    int E = 0;
+    int O = 0; 
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+        cin >> N;
+    while(N != 0){
+        if(N%2 == 0){
+            E = E+1;
+        }else{
+            O = O+1;
+        }
+        cout << "Enter an integer: ";
+        cin >> N;
+    }
+    
+    cout << "#Even numbers = " << E << endl;
+    cout << "#Odd numbers = " << O;
+    
     return 0;
+
 }
--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-
+double deg2rad(double x){
+    double r;
+    r = (x*M_PI)/180;
+    return r;
+}
+double rad2deg(double x){
+    double d;
+    d = (180*x)/M_PI;
+    return d;
+}
+double findXComponent(double l1, double l2, double a1, double a2){
+    double xc;
+    xc = l1*cos(a1) + l2*cos(a2);
+    return xc;
+}
+double findYComponent(double l1, double l2, double a1, double a2){
+    double yc;
+    yc = l1*sin(a1) + l2*sin(a2);
+    return yc;
+}
+double pythagoras(double xc, double yc){
+    double pyth;
+    pyth = sqrt(pow(xc, 2)+ pow(yc, 2));
+    return pyth;
+}
+void showResult(double a, double b){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << "\n";
+    cout << "Length of the resultant vector = " << a << endl;
+    cout << "Direction of the resultant vector (deg) = " << b << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
